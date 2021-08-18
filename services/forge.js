ce6341670910929831a8ad13d3c9270038440395
Petr Broz <ipetrbroz@gmail.com>
2021-08-18 13:15:56 +0200

    Using forge-server-utils instead of forge-apis.

--- services/forge.js
+++ services/forge.js
@@ -1,4 +1,4 @@
-const { AuthClientTwoLegged, BucketsApi, ObjectsApi, DerivativesApi } = require('forge-apis');
+const { AuthenticationClient, DataManagementClient, ModelDerivativeClient, DataRetentionPolicy, urnify } = require('forge-server-utils');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
 if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
@@ -6,57 +6,29 @@ if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
     process.exit(1);
 }
 const BUCKET = FORGE_BUCKET || `${FORGE_CLIENT_ID.toLowerCase()}-basic-app`;
-const INTERNAL_TOKEN_SCOPES = ['bucket:read', 'bucket:create', 'data:read', 'data:write', 'data:create'];
-const PUBLIC_TOKEN_SCOPES = ['viewables:read'];
 
-let _tokenCache = new Map();
-
-async function _getAccessToken(scopes) {
-    const key = scopes.join(',');
-    let token = _tokenCache.get(key);
-    if (!token || token.expires_at < Date.now()) {
-        const client = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, scopes);
-        token = await client.authenticate();
-        token.expires_at = Date.now() + token.expires_in * 1000;
-        _tokenCache.set(key, token);
-    }
-    return {
-        access_token: token.access_token,
-        // token_type: token.token_type,
-        expires_in: Math.round((token.expires_at - Date.now()) / 1000)
-    };
-}
-
-function _urnify(id) {
-    return Buffer.from(id).toString('base64').replace(/=/g, '');
-}
+let authenticationClient = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
+let dataManagementClient = new DataManagementClient({ client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET });
+let modelDerivativeClient = new ModelDerivativeClient({ client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET });
 
 async function getPublicToken() {
-    return _getAccessToken(PUBLIC_TOKEN_SCOPES);
+    return authenticationClient.authenticate(['viewables:read']);
 }
 
 async function listModels() {
-    const token = await _getAccessToken(INTERNAL_TOKEN_SCOPES);
-    let response = await new ObjectsApi().getObjects(BUCKET, { limit: 64 }, null, token);
-    let objects = response.body.items;
-    while (response.body.next) {
-        const startAt = new URL(response.body.next).searchParams.get('startAt');
-        response = await new ObjectsApi().getObjects(BUCKET, { limit: 64, startAt }, null, token);
-        objects = objects.concat(response.body.items);
-    }
+    const objects = await dataManagementClient.listObjects(BUCKET);
     return objects.map(obj => ({
         name: obj.objectKey,
-        urn: _urnify(obj.objectId)
+        urn: urnify(obj.objectId)
     }));
 }
 
 async function ensureBucketExists() {
-    const token = await _getAccessToken(INTERNAL_TOKEN_SCOPES);
     try {
-        await new BucketsApi().getBucketDetails(BUCKET, null, token);
+        await dataManagementClient.getBucketDetails(BUCKET);
     } catch (err) {
         if (err.statusCode === 404) {
-            await new BucketsApi().createBucket({ bucketKey: BUCKET, policyKey: 'temporary' }, {}, null, token);
+            await dataManagementClient.createBucket(BUCKET, DataRetentionPolicy.Temporary);
         } else {
             throw err;
         }
@@ -64,21 +36,8 @@ async function ensureBucketExists() {
 }
 
 async function uploadModel(objectName, buffer, rootFilename) {
-    const token = await _getAccessToken(INTERNAL_TOKEN_SCOPES);
-    const response = await new ObjectsApi().uploadObject(BUCKET, objectName, buffer.byteLength, buffer, {}, null, token);
-    const job = {
-        input: {
-            urn: _urnify(response.body.objectId)
-        },
-        output: {
-            formats: [{ type: 'svf', views: ['2d', '3d'] }]
-        }
-    };
-    if (rootFilename) {
-        job.input.compressedUrn = true;
-        job.input.rootFilename = rootFilename;
-    }
-    await new DerivativesApi().translate(job, {}, null, token);
+    const object = await dataManagementClient.uploadObject(BUCKET, objectName, 'application/octet-stream', buffer);
+    await modelDerivativeClient.submitJob(urnify(object.objectId), [{ type: 'svf', views: ['2d', '3d'] }], rootFilename);
 }
 
 module.exports = {
