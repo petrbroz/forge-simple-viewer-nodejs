4fc777600ad16c31f75da972474d54723cb4c1eb
Petr Broz <ipetrbroz@gmail.com>
2021-08-20 13:02:09 +0200

    Adding experimental SDK wrapper.

--- services/forge.js
+++ services/forge.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { AuthClientTwoLegged, BucketsApi, ObjectsApi, DerivativesApi } = require('forge-apis');
+const { AuthenticationClient, OSSClient, ModelDerivativeClient, urnify } = require('simpler-forge-apis');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
 if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
@@ -7,75 +7,37 @@ if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
     process.exit(1);
 }
 const BUCKET = FORGE_BUCKET || `${FORGE_CLIENT_ID.toLowerCase()}-basic-app`;
-const INTERNAL_TOKEN_SCOPES = ['bucket:read', 'bucket:create', 'data:read', 'data:write', 'data:create'];
-const PUBLIC_TOKEN_SCOPES = ['viewables:read'];
 
-let internalAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, INTERNAL_TOKEN_SCOPES, true);
-let publicAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, PUBLIC_TOKEN_SCOPES, true);
+const authenticationClient = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
+const ossClient = new OSSClient({ client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET });
+const modelDerivativeClient = new ModelDerivativeClient({ client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET });
 
-const urnify = (id) => Buffer.from(id).toString('base64').replace(/=/g, '');
-
-async function getInternalToken() {
-    if (!internalAuthClient.isAuthorized()) {
-        await internalAuthClient.authenticate();
-    }
-    return internalAuthClient.getCredentials();
-}
-
-async function getPublicToken() {
-    if (!publicAuthClient.isAuthorized()) {
-        await publicAuthClient.authenticate();
-    }
-    return publicAuthClient.getCredentials();
-}
-
-async function ensureBucketExists() {
-    const token = await getInternalToken();
+async function _ensureBucketExists() {
     try {
-        await new BucketsApi().getBucketDetails(BUCKET, null, token);
+        await ossClient.getBucketDetails(BUCKET);
     } catch (err) {
         if (err.statusCode === 404) {
-            await new BucketsApi().createBucket({ bucketKey: BUCKET, policyKey: 'temporary' }, {}, null, token);
+            await ossClient.createBucket(BUCKET, 'temporary');
         } else {
             throw err;
         }
     }
 }
 
+async function getPublicToken() {
+    return authenticationClient.authenticate(['viewables:read']);
+}
+
 async function listModels() {
-    await ensureBucketExists(); // Remove this if we can assume the bucket to exist
-    const token = await getInternalToken();
-    let response = await new ObjectsApi().getObjects(BUCKET, { limit: 64 }, null, token);
-    let objects = response.body.items;
-    while (response.body.next) {
-        const startAt = new URL(response.body.next).searchParams.get('startAt');
-        response = await new ObjectsApi().getObjects(BUCKET, { limit: 64, startAt }, null, token);
-        objects = objects.concat(response.body.items);
-    }
-    return objects.map(obj => ({
-        name: obj.objectKey,
-        urn: urnify(obj.objectId)
-    }));
+    await _ensureBucketExists(); // Remove this if we can assume the bucket to exist
+    const objects = await ossClient.listObjects(BUCKET);
+    return objects.map(obj => ({ name: obj.objectKey, urn: urnify(obj.objectId) }));
 }
 
 async function uploadModel(objectName, filePath, rootFilename) {
-    await ensureBucketExists(); // Remove this if we can assume the bucket to exist
-    const token = await getInternalToken();
-    const buffer = fs.readFileSync(filePath);
-    const response = await new ObjectsApi().uploadObject(BUCKET, objectName, buffer.byteLength, buffer, {}, null, token);
-    const job = {
-        input: {
-            urn: urnify(response.body.objectId)
-        },
-        output: {
-            formats: [{ type: 'svf', views: ['2d', '3d'] }]
-        }
-    };
-    if (rootFilename) {
-        job.input.compressedUrn = true;
-        job.input.rootFilename = rootFilename;
-    }
-    await new DerivativesApi().translate(job, {}, null, token);
+    await _ensureBucketExists(); // Remove this if we can assume the bucket to exist
+    const obj = await ossClient.uploadObject(BUCKET, objectName, fs.readFileSync(filePath));
+    await modelDerivativeClient.submitJob(urnify(obj.objectId), [{ type: 'svf', views: ['2d', '3d'] }], rootFilename);
 }
 
 module.exports = {
