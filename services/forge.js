ebc2a250a161fa8a6b31f23e5b0ae0ec6f06c845
Petr Broz <ipetrbroz@gmail.com>
2021-11-09 10:39:12 +0100

    Cleaned up the multi-model sample.

--- services/forge.js
+++ services/forge.js
@@ -1,26 +1,14 @@
-const fs = require('fs');
-const { AuthClientTwoLegged, BucketsApi, ObjectsApi, DerivativesApi } = require('forge-apis');
+const { AuthClientTwoLegged } = require('forge-apis');
 
-const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
+const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
 if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
     console.warn('Missing some of the environment variables.');
     process.exit(1);
 }
-const BUCKET = FORGE_BUCKET || `${FORGE_CLIENT_ID.toLowerCase()}-basic-app`;
-const INTERNAL_TOKEN_SCOPES = ['bucket:read', 'bucket:create', 'data:read', 'data:write', 'data:create'];
+
 const PUBLIC_TOKEN_SCOPES = ['viewables:read'];
 
-let internalAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, INTERNAL_TOKEN_SCOPES, true);
 let publicAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, PUBLIC_TOKEN_SCOPES, true);
-
-const urnify = (id) => Buffer.from(id).toString('base64').replace(/=/g, '');
-
-async function getInternalToken() {
-    if (!internalAuthClient.isAuthorized()) {
-        await internalAuthClient.authenticate();
-    }
-    return internalAuthClient.getCredentials();
-}
 
 async function getPublicToken() {
     if (!publicAuthClient.isAuthorized()) {
@@ -29,57 +17,6 @@ async function getPublicToken() {
     return publicAuthClient.getCredentials();
 }
 
-async function ensureBucketExists() {
-    const token = await getInternalToken();
-    try {
-        await new BucketsApi().getBucketDetails(BUCKET, null, token);
-    } catch (err) {
-        if (err.statusCode === 404) {
-            await new BucketsApi().createBucket({ bucketKey: BUCKET, policyKey: 'temporary' }, {}, null, token);
-        } else {
-            throw err;
-        }
-    }
-}
-
-async function listModels() {
-    await ensureBucketExists(); // Remove this if we can assume the bucket to exist
-    const token = await getInternalToken();
-    let response = await new ObjectsApi().getObjects(BUCKET, { limit: 64 }, null, token);
-    let objects = response.body.items;
-    while (response.body.next) {
-        const startAt = new URL(response.body.next).searchParams.get('startAt');
-        response = await new ObjectsApi().getObjects(BUCKET, { limit: 64, startAt }, null, token);
-        objects = objects.concat(response.body.items);
-    }
-    return objects.map(obj => ({
-        name: obj.objectKey,
-        urn: urnify(obj.objectId)
-    }));
-}
-
-async function uploadModel(objectName, filePath, rootFilename) {
-    await ensureBucketExists(); // Remove this if we can assume the bucket to exist
-    const token = await getInternalToken();
-    const buffer = fs.readFileSync(filePath);
-    const response = await new ObjectsApi().uploadObject(BUCKET, objectName, buffer.byteLength, buffer, {}, null, token);
-    const job = {
-        input: {
-            urn: urnify(response.body.objectId)
-        },
-        output: {
-            formats: [{ type: 'svf', views: ['2d', '3d'] }]
-        }
-    };
-    if (rootFilename) {
-        job.input.compressedUrn = true;
-        job.input.rootFilename = rootFilename;
-    }
-    await new DerivativesApi().translate(job, {}, null, token);
-}
-
 module.exports = {
-    getPublicToken,
-    listModels,
-    uploadModel
+    getPublicToken
 };
