bc103b4aa68e2709b6b1a3e84daa9d8678675973
Petr Broz <petr.broz@autodesk.com>
2022-09-19 11:01:03 +0200

    Added test scene with COLLADA loader.

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,4 +1,5 @@
 import { initViewer, loadModel } from './viewer.js';
+// import { ColladaLoader } from 'three/addons/loaders/ColladaLoader.js';
 
 initViewer(document.getElementById('preview')).then(viewer => {
     const urn = window.location.hash?.substring(1);
@@ -17,7 +18,7 @@ async function setupModelSelection(viewer, selectedUrn) {
         dropdown.innerHTML = models.map(model => `<option value=${model.urn} ${model.urn === selectedUrn ? 'selected' : ''}>${model.name}</option>`).join('\n');
         dropdown.onchange = () => {
             window.location.hash = dropdown.value;
-            loadModel(viewer, dropdown.value).then(() => addTestScene(viewer));
+            loadModel(viewer, dropdown.value).then(() => addTestSceneCollada(viewer));
         }
         if (dropdown.value) {
             dropdown.onchange();
@@ -28,7 +29,7 @@ async function setupModelSelection(viewer, selectedUrn) {
     }
 }
 
-async function addTestScene(viewer) {
+async function addTestSceneBasic(viewer) {
     const scene = new THREE.Scene();
     const normalsMaterial =  new THREE.MeshNormalMaterial({ side: THREE.FrontSide });
     const sphere = new THREE.IcosahedronGeometry(12.0, THREE.REVISION <= 71 ? 4 : 8);
@@ -36,4 +37,23 @@ async function addTestScene(viewer) {
     scene.add(new THREE.Mesh(sphere, normalsMaterial));
     scene.add(new THREE.Mesh(torus, normalsMaterial));
     viewer.companions.add('my-scene', scene, Autodesk.Viewing.CompanionType.THREE_SCENE_BEFORE);
+}
+
+async function addTestSceneCollada(viewer) {
+    const loader = new THREE.ColladaLoader();
+    loader.load('/abb_irb52_7_120.dae', function (collada) {
+        let dae = collada.scene;
+        dae.traverse(function (child) {
+            if (child.isMesh) {
+                child.material.flatShading = true; // model does not have normals
+            }
+        });
+        dae.scale.x = dae.scale.y = dae.scale.z = 10.0;
+        dae.updateMatrix();
+        const grid = new THREE.GridHelper(20, 20, 0x888888, 0x444444);
+        const scene = new THREE.Scene();
+        scene.add(grid);
+        scene.add(dae);
+        viewer.companions.add('my-scene-collada', scene, Autodesk.Viewing.CompanionType.THREE_SCENE_BEFORE);
+    });
 }
